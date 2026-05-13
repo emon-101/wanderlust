c39d001b42bfcdab2b241822b90ca9d4a0c0e00c
emon-101 <emon26.ugv@gmail.com>
2026-05-13 12:18:52 +0600

    feat: implement form submission for adding a destination

--- src/app/add-destination/page.jsx
+++ src/app/add-destination/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Button,
   FieldError,
@@ -11,11 +12,27 @@ import {
 } from "@heroui/react";
 
 const AddDestinationPage = () => {
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const destination = Object.fromEntries(formData.entries());
+
+       const res = await fetch('http://localhost:5000/destination', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(destination)
+        })
+
+        const data = await res.json();
+        console.log(data);
+    }
   return (
     <div className="lg:w-1/2 mx-auto px-4 my-8">
       <h1 className="text-xl font-bold my-4 text-center">Add destination</h1>
       <Card>
-      <form className="p-10 space-y-8">
+      <form onSubmit={onSubmit} className="p-10 space-y-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Destination Name */}
           <div className="md:col-span-2">
